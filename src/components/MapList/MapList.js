7214a70c2619c0e09b84b0f30da2447e120f4f8f
Gőz Attila <goz.attila.1977@gmail.com>
2021-01-13 19:23:16 +0100

    Map fetch

--- src/components/MapList/MapList.js
+++ src/components/MapList/MapList.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import "./MapList.css";
+
 import CountryList from "../CountryList/CountryList";
 import TopButtons from "../TopButtons/TopButtons";
 import WorldMap from "../WorldMap/WorldMap";
 
-import "./MapList.css";
-
 function MapList() {
+  const [mapCountries, setMapCountries] = useState([]);
+  const [worldData, setWorldData] = useState({});
+  const [perCapita, setPerCapita] = useState(false);
+  const [caseType, setCaseType] = useState("cases");
+
+  useEffect(() => {
+    const getCountriesData = () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          setMapCountries(data);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  useEffect(() => {
+    const getWorldData = () => {
+      fetch("https://disease.sh/v3/covid-19/all")
+        .then((response) => response.json())
+        .then((data) => {
+          setWorldData(data);
+        });
+    };
+
+    getWorldData();
+  }, []);
+
   return (
     <div className="mapList">
       <div className="mapList__left">
         <TopButtons />
-        <WorldMap />
+        <WorldMap
+          worldData={worldData}
+          mapCountries={mapCountries}
+          caseType={caseType}
+          perCapita={perCapita}
+        />
       </div>
       <div className="mapList__right">
-        <CountryList />
+        <CountryList
+          mapCountries={mapCountries}
+          caseType={caseType}
+          perCapita={perCapita}
+        />
       </div>
     </div>
   );
