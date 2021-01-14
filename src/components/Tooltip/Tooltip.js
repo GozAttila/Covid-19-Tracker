ea261eb1ebe6b84826d81461ce148cece77a20cf
Gőz Attila <goz.attila.1977@gmail.com>
2021-01-14 08:08:18 +0100

    Finishing Tooltip design

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -2,8 +2,47 @@ import React from "react";
 
 import "./Tooltip.css";
 
-function Tooltip() {
-  return <div></div>;
+function Tooltip({ tooltipData }) {
+  const {
+    flag,
+    country,
+    population,
+    cases,
+    active,
+    recovered,
+    deaths,
+  } = tooltipData;
+
+  return (
+    <div className="tooltip">
+      <div className="tooltip__header">
+        <img className="tooltip__flag" src={flag} alt="country flag" />
+        <div className="tooltip__country">{country}</div>
+        <div className="tooltip__population">Population:</div>
+        <div className="tooltip__population">{population}</div>
+      </div>
+
+      <div className="tooltip__data">
+        <div className="tooltip__title">Cases:</div>
+        <div className="tooltip__cases">
+          <div>All:</div>
+          <div>{cases}</div>
+        </div>
+        <div className="tooltip__cases">
+          <div>Active:</div>
+          <div>{active}</div>
+        </div>
+        <div className="tooltip__cases">
+          <div>Recovered:</div>
+          <div>{recovered}</div>
+        </div>
+        <div className="tooltip__cases">
+          <div>Deaths:</div>
+          <div>{deaths}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Tooltip;
--- src/components/WorldMap/WorldMap.js
+++ src/components/WorldMap/WorldMap.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Tooltip from "../Tooltip/Tooltip";
 
 import "./WorldMap.css";
 
 function WorldMap() {
+  const [tooltipData, setTooltipData] = useState({
+    flag:
+      "https://images.squarespace-cdn.com/content/v1/5506acb1e4b0b39d3c22d329/1430773739856-CGYOUJP6S653W4B13HVY/ke17ZwdGBToddI8pDm48kGwkjuynw4B8tYYHsellrnlZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxHD-n7kw2ta02ApS8KVYqRgwdRjd54i2ObM4fy-YBd1g4Ns4l_SJho4nLemRk_BII/image-asset.jpeg?format=500w",
+    country: "Worldwide",
+    cases: 123455,
+    active: 2345,
+    recovered: 5678,
+    deaths: 123,
+    population: 7654321,
+  });
+
   return (
     <>
       <div id="map">WorldMap</div>
-      {/* <div>Tooltip</div> */}
+      {/* <Tooltip tooltipData={tooltipData} /> */}
     </>
   );
 }
