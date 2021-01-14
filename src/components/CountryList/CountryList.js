3d8335cc0e335877c48ee785170728568dcaa628
Gőz Attila <goz.attila.1977@gmail.com>
2021-01-14 06:48:54 +0100

    Finishing side list design Add sort logic for side list Add listing function for side list

--- src/components/CountryList/CountryList.js
+++ src/components/CountryList/CountryList.js
@@ -1,23 +1,32 @@
 import React from "react";
+import numeral from "numeral";
 
 import "./CountryList.css";
 
-function CountryList() {
+function CountryList({ mapCountries, caseType, perCapita }) {
+  const sortData = (data) => {
+    let sortedData = [...data];
+    sortedData.sort((a, b) => {
+      if (a[caseType] > b[caseType]) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    return sortedData;
+  };
+
   return (
     <table className="countryList">
       <tbody>
-        <tr>
-          <td>country 1</td>
-          <td>12345</td>
-        </tr>
-        <tr>
-          <td>country 2</td>
-          <td>12345</td>
-        </tr>
-        <tr>
-          <td>country 3</td>
-          <td>12345</td>
-        </tr>
+        {sortData(mapCountries).map((country) => (
+          <tr key={country.country}>
+            <td>{country.country}</td>
+            <td>
+              <strong>{numeral(country[caseType]).format("0,0")}</strong>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
